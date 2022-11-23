c58d8f5fbccc805cf23129a1fdddc2200a9f9989
brewea <56275753+brewea@users.noreply.github.com>
2022-11-23 18:29:52 -0500

    working auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator, Button, Heading } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
+	// ...
+	return (
+		<div className="App">
+			<Heading level={1}>Hello {user.username}</Heading>
+			<Button onClick={signOut}>Sign out</Button>
+			<h2>Amplify Todos</h2>
+		</div>
+	);
 }
 
-export default App;
+export default withAuthenticator(App);
